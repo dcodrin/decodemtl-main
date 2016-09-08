fcd2b2abdd9b42dd2f28abac03079c114e70d2fd
--replace-all <diaconu.codrin@gmail.com>
2016-09-07 21:09:35 -0400

    Added Page Transitions.

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,26 @@
 import React from 'react';
+import RouteTransition from 'react-easy-transition';
 
 import SiteNav from './navigation/SiteNav';
 
-const App = React.createClass({
-    propTypes: {
-        children: React.PropTypes.element
-    },
-    render() {
-        return (
-            <div className="App">
-                <SiteNav />
-                {this.props.children}
-            </div>
-        );
-    }
-});
+const App = (props) => {
+    return (
+        <div className="App">
+            <SiteNav />
+            <RouteTransition
+                path={props.location.pathname}
+                initialStyle={{opacity: 0}}
+                transition="opacity 0.2s ease-in"
+                finalStyle={{opacity: 1}}
+            >
+                {props.children}
+            </RouteTransition>
+        </div>
+    );
+};
 
-
+App.propTypes = {
+    children: React.PropTypes.element
+};
 
 export default App;
