0510cdf559dcf957d29c57dd6c48a78c7ca0515b
Kevin Khoury <kev_khoury@me.com>
2016-09-13 17:30:22 -0400

    added images to full-time bootcamp testimonials

--- src/components/pages/wd_bootcamp/testimonials.js
+++ src/components/pages/wd_bootcamp/testimonials.js
@@ -1,33 +1,32 @@
-import ziad from '../../../assets/images/profile-team-ziad.jpg';
+import kayla from '../../../assets/images/testimonial-kayla-henig.jpg';
+import nathaniel from '../../../assets/images/testimonial-nathan-kitzke.jpg';
+import johny from '../../../assets/images/testimonial-johny-duval.jpg';
+import scott from '../../../assets/images/testimonial-scott-morin.jpg';
+import marie from '../../../assets/images/testimonial-marie-godon.jpg';
 export default [
     {
         fullName: 'Kayla Henig, Web Developer at Autodesk',
         text: 'DecodeMTL\'s full-time bootcamp provided me with the skills and confidence to find my first job in the industry and begin my new career as a Web Developer.',
-        img: ziad
+        img: kayla
     },
     {
         fullName: 'Nathaniel Kitzke, Front-End Developer at ROI',
         text: 'After only two months, it allowed me to feel confident in approaching potential employers knowing that I could say "yes" to most of their questions.',
-        img: ziad
+        img: nathaniel
     },
     {
         fullName: 'Johny Duval, Product Manager at SweetIQ',
         text: 'Seeing what I was able to achieve in 8 short weeks, DecodeMTL has given me the tools and the confidence to learn any programming language that I encounter.',
-        img: ziad
+        img: johny
     },
     {
         fullName: 'Scott Morin, Front-End Developer at App Direct',
         text: 'DecodeMTL has allowed to successfully change my career from teacher, and part-time web designer to a full-time full stack web developer!',
-        img: ziad
+        img: scott
     },
     {
         fullName: 'Marie Godon, Web Developer at Maya HTT',
         text: 'I would definitely recommend taking the full-time course at DecodeMTL. I found a job just a few weeks after the program with a competitive salary and nice benefits.',
-        img: ziad
-    },
-    {
-        fullName: 'Mark Crisp, Software Developer at SweetIQ',
-        text: 'This is a great course condensed into an arduous 2 months. You won\'t learn everything that an employer could possibly ask for, but what you do get is a well rounded foundation that will give you a start in the business.',
-        img: ziad
+        img: marie
     },
 ];
