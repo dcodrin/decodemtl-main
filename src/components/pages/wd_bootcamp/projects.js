41fb11c33e7b81ee86bfa32f6fe8540a93e97ced
Kevin Khoury <kev_khoury@me.com>
2016-09-13 21:46:39 -0400

    Added project titles for full-time bootcamp

--- src/components/pages/wd_bootcamp/projects.js
+++ src/components/pages/wd_bootcamp/projects.js
@@ -5,22 +5,22 @@ import p4 from '../../../assets/images/project-wd-bootcamp-4.jpg'
 
 export default [
     {
-        title: 'Cool Project',
+        title: 'Citizen',
         description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores cupiditate delectus deserunt dolor dolorum eaque fuga impedit magni mollitia neque nisi nostrum provident quisquam, repudiandae saepe sed vel veniam voluptas?',
         img: p1
     },
     {
-        title: 'Cooler Project',
+        title: 'BayBay',
         description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet explicabo optio perferendis placeat voluptatibus? Architecto eius expedita laboriosam molestias mollitia nulla omnis pariatur quasi, quisquam repellendus, sed tempore temporibus, ullam.',
         img: p2
     },
     {
-        title: 'The Coolest Project',
+        title: 'Yocaza',
         description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium ad adipisci culpa distinctio dolorum earum eos et facilis hic in inventore ipsam, magni nisi optio porro quam quidem reiciendis, tempore.',
         img: p3
     },
     {
-        title: 'Uber Cool Project',
+        title: 'Weather Sequencer',
         description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda blanditiis consectetur cumque dignissimos, fugit maxime nam non perferendis quisquam reiciendis. Earum, nihil, officia. Accusamus aliquid deserunt doloribus eius ipsam obcaecati.',
         img: p4
     }
