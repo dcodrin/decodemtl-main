908243c0ffef5698be18236271f369b72272bbec
Kevin Khoury <kev_khoury@me.com>
2016-09-14 11:34:09 -0400

    Added descriptions to Full-Time Bootcamp projects

--- src/components/pages/wd_bootcamp/projects.js
+++ src/components/pages/wd_bootcamp/projects.js
@@ -6,22 +6,22 @@ import p4 from '../../../assets/images/project-wd-bootcamp-4.jpg'
 export default [
     {
         title: 'Citizen',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores cupiditate delectus deserunt dolor dolorum eaque fuga impedit magni mollitia neque nisi nostrum provident quisquam, repudiandae saepe sed vel veniam voluptas?',
+        description: 'A single page political accountability app. Using React, nodeJS, jQuery, MongoDB, APIs, Parse, HTML and CSS, Citizen is a responsive web-based app to inform you who represents you in government, helps you find out what they\'re doing in your name and enables you to take action.',
         img: p1
     },
     {
+        title: 'Weather Sequencer',
+        description: 'This is a single-page application that brings together music and weather. Using the HTML5 Audio API, forecast.io\'s weather API, Google\'s Geocoding API and some jQuery magic, this app lets you create and share melodies that will be affected by the local weather',
+        img: p4
+    },
+    {
         title: 'BayBay',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet explicabo optio perferendis placeat voluptatibus? Architecto eius expedita laboriosam molestias mollitia nulla omnis pariatur quasi, quisquam repellendus, sed tempore temporibus, ullam.',
+        description: 'The word "Baby" is in thousands of songs. The BayBay web app is an awesome game in which you hear a two second snippet of a song with the word "Baby" and you have to guess the artist and song title.',
         img: p2
     },
     {
         title: 'Yocaza',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium ad adipisci culpa distinctio dolorum earum eos et facilis hic in inventore ipsam, magni nisi optio porro quam quidem reiciendis, tempore.',
+        description: 'The simplest way for Real Estate Agents to generate a feature sheet. Simply drag and drop some images, write-up your text, and hit print. The Yocaza feature sheet generator is a web app to create one page feature sheets.',
         img: p3
     },
-    {
-        title: 'Weather Sequencer',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda blanditiis consectetur cumque dignissimos, fugit maxime nam non perferendis quisquam reiciendis. Earum, nihil, officia. Accusamus aliquid deserunt doloribus eius ipsam obcaecati.',
-        img: p4
-    }
 ];
