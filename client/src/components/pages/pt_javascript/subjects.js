756f57718e3cb91b006c440d6022e2e29cd2a390
Kevin Khoury <kev_khoury@me.com>
2016-09-21 17:36:37 -0400

    update js course icons

--- client/src/components/pages/pt_javascript/subjects.js
+++ client/src/components/pages/pt_javascript/subjects.js
@@ -1,21 +1,21 @@
-import iconApi from '../../../assets/icons/icon-api.svg';
-import iconStrategy from '../../../assets/icons/icon-strategy.svg';
-import iconLearn from '../../../assets/icons/icon-learn.svg';
+import iconMobileData from '../../../assets/icons/icon-mobile-data.svg';
+import iconTools from '../../../assets/icons/icon-tools.svg';
+import iconAlgorithm from '../../../assets/icons/icon-algorithm.svg';
 
 export default [
     {
         title: 'Prototypes',
         text: 'Understand JavaScript\'s prototype based object model, and how to compose the perfect object to meet your needs.',
-        icon: iconApi
+        icon: iconMobileData
     },
     {
         title: 'Functions',
         text: 'Become a functional magician by learning to use closures in JavaScript and becoming fluent in higher order functions.',
-        icon: iconLearn
+        icon: iconAlgorithm
     },
     {
         title: 'Async',
         text: 'Master JavaScript\'s unique asynchronous programming model, and learn to manage nested callbacks like a pro.',
-        icon: iconStrategy
+        icon: iconTools
     },
 ];
