f3935c679ab2f86ea4409c8d3457015a681bfcb3
Kevin Khoury <kev_khoury@me.com>
2016-09-21 11:26:10 -0400

    added text to confirmation page

--- client/src/components/pages/confirmation/index.js
+++ client/src/components/pages/confirmation/index.js
@@ -4,8 +4,9 @@ const Confirmation = (props) => {
     return (
         <section className="module">
             <div className="wrapper">
-                <h2 className="module-title">Thank you!</h2>
-                <p>Yahoo!</p>
+                <h2 className="module-title">Success!</h2>
+                <p>Your application has been received. Our team is currently reviewing it and will get back to you within 48 hours (not including weekends or holidays).</p>
+                <p>If you have any questions in the mean time, please <a href="mailto:hello@decodemtl.com">email us</a>.</p>
             </div>
         </section>
     );
