3589111b882031e3febd12066c59d7decdc8d4b7
--replace-all <diaconu.codrin@gmail.com>
2016-09-23 14:52:20 -0400

    Updated PartnersLogos links and footer logo.

--- client/src/components/modules/PartnersLogos.js
+++ client/src/components/modules/PartnersLogos.js
@@ -5,6 +5,10 @@ import React from 'react';
 const req = require.context("../../assets/images/partner-logos", true, /^.*\.png$/i);
 
 const PartnersLogos = (props) => {
+    let employerLink = '/employers';
+    if (window.location.pathname === '/employers') {
+        employerLink = 'mailto:hello@decodemtl.com?subject=Tell me more about your students!&body=Hello Decode! Please let me know more about your awesome students!'
+    }
     return (
         <section className="module partner-logos-module module-boxed-medium">
             <div className="wrapper">
@@ -20,7 +24,7 @@ const PartnersLogos = (props) => {
                 </div>
                 {/*<!-- /.partner-logo-container -->*/}
                 <div className="link-more text-body-small">
-                    <a href="mailto:hello@decodemtl.com?subject=Tell me more about your students!&body=Hello Decode! Please let me know more about your awesome students!">Join
+                    <a href={employerLink}>Join
                         our growing network of hiring partners<span className="fa fa-caret-right"
                                                                     aria-hidden="true"/></a>
                 </div>
