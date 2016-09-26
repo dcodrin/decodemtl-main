2dd1c9408da0054eba7f6e2440056014fa68d569
--replace-all <diaconu.codrin@gmail.com>
2016-09-26 16:42:34 -0400

    Added <sup> to courses page.

--- client/src/components/tables/TuitionDates.js
+++ client/src/components/tables/TuitionDates.js
@@ -14,7 +14,9 @@ const TuitionDates = React.createClass({
     render() {
         return (
             <div className={this.props.price ? 'row' : 'row full'}>
-                <div className="cell">{this.props.startDate}—{this.props.endDate}</div>
+                <div
+                    className="cell">{this.props.startDate.replace(/[a-z]+$/i, '')}<sup>{this.props.startDate.match(/\d+(.*)/)[1]}</sup>-{this.props.endDate.replace(/[a-z]+$/i, '')}<sup>{this.props.endDate.match(/\d+(.*)/)[1]}</sup>
+                </div>
                 <div className="cell">{this.props.days}</div>
                 <div className="cell">{this.props.hours}</div>
                 <div className="cell">{this.props.price ? this._formatPrice(this.props.price) : 'Full'}</div>
