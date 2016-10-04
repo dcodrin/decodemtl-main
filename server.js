8123380029e731eafbb6c216513a4b90eac7e325
--replace-all <diaconu.codrin@gmail.com>
2016-10-04 11:49:22 -0400

    Deploy Build

--- server.js
+++ server.js
@@ -276,6 +276,8 @@ app.post('/api/contact', (req, res) => {
 
 });
 
+//Load index on initial request
+
 app.get('/*', (req, res) => {
     res.sendFile(path.resolve(__dirname + '/build', 'index.html'));
 });
